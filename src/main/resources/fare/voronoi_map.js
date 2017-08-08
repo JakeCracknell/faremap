9d30fb04316c079a6440f58e047f33f6e7852c37
Jake Cracknell <jc4512@imperial.ac.uk>
2017-08-08 22:09:48 +0100

    showing station name on overlay

--- src/main/resources/fare/voronoi_map.js
+++ src/main/resources/fare/voronoi_map.js
@@ -32,10 +32,8 @@ voronoiMap = function(map, url, initialSelections) {
 
     d3.select('#selected h1')
       .html('')
-      .append('a')
-        .text(point.name)
-        .attr('href', point.url)
-        .attr('target', '_blank')
+      .append('p')
+      .text(point.stationId + " " + point.stationName)
   }
 
   var drawPointTypeSelection = function() {
