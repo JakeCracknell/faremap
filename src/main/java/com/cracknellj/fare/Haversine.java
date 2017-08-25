6d505e4ddb19fb6dfd3e8311c240e377d6b60cd9
Jake Cracknell <jc4512@imperial.ac.uk>
2017-08-25 19:07:40 +0100

    journey value analysis

---
+++ src/main/java/com/cracknellj/fare/Haversine.java
@@ -0,0 +1,24 @@
+package com.cracknellj.fare;
+
+public class Haversine {
+    private static final int EARTH_RADIUS = 6371; // Approx Earth radius in KM
+
+    public static double distance(double startLat, double startLong,
+                                  double endLat, double endLong) {
+
+        double dLat  = Math.toRadians((endLat - startLat));
+        double dLong = Math.toRadians((endLong - startLong));
+
+        startLat = Math.toRadians(startLat);
+        endLat   = Math.toRadians(endLat);
+
+        double a = haversin(dLat) + Math.cos(startLat) * Math.cos(endLat) * haversin(dLong);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS * c; // <-- d
+    }
+
+    private static double haversin(double val) {
+        return Math.pow(Math.sin(val / 2), 2);
+    }
+}
