acdb8d2adce6a30334e3f863a58165b5ffff28be
Jake Cracknell <jc4512@imperial.ac.uk>
2017-08-25 20:44:43 +0100

    colour based on fare-price-selector dropdown

--- src/main/resources/fare/voronoi_map.js
+++ src/main/resources/fare/voronoi_map.js
@@ -65,7 +65,7 @@ voronoiMap = function(map, url) {
 
   var setupDisplayOptionsPanel = function() {
     addShowHideEventsTo('#selections')
-    d3.selectAll('#mode-toggles input[type=checkbox]')
+    d3.selectAll('#mode-toggles, #fare-type-toggles')
         .on("change", drawWithLoading);
   }
 
@@ -143,17 +143,30 @@ voronoiMap = function(map, url) {
       return "M" + point.cell.join("L") + "Z";
     }
 
-    var getFillColourForPoint = function(point) {
-      if (point.fares.length == 0) return 'transparent';
-      var proportionAlongScale = Math.max.apply(Math,point.fares.map(function(f){return f.price;})) / 8
-      var hue=((1-proportionAlongScale)*120).toString(10);
+    var getFillColourForAdjustedPrice = function(price) {
+      if (price == Infinity || !price) return 'transparent';
+      var hue=((1-price)*120).toString(10);
       return ["hsl(",hue,",100%,50%)"].join("");
     }
 
+    var fareSelectorElement = document.getElementById('fare-price-selector');
+    var fareSelectorFunction;
+    switch (fareSelectorElement.options[fareSelectorElement.selectedIndex].id) {
+        case 'low':
+            fareSelectorFunction = fs => Math.min.apply(Math, fs.map(function(f){return f.price;}))
+            break;
+        case 'high':
+            fareSelectorFunction = fs => Math.max.apply(Math, fs.map(function(f){return f.price;}))
+            break;
+        default:
+            fareSelectorFunction = fs => Math.min.apply(Math, fs.filter(d => d.isDefaultRoute).map(function(f){return f.price;}))
+            break;
+    }
+    var averagePrice = 5.0
     svgPoints.append("path")
       .attr("class", "point-cell")
       .attr("d", buildPathFromPoint)
-      .style('fill', function(d) { return getFillColourForPoint(d) } )
+      .style('fill', function(d) { return getFillColourForAdjustedPrice(fareSelectorFunction(d.fares) / averagePrice) })
       .on('click', selectPointForFareQuery)
       .on('mouseover', showMouseOverInformationForPoint)
       .classed("selected", function(d) { return lastSelectedPoint == d} );
