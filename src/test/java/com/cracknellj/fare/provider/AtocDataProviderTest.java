8684566764814506ae64eb30f0a3cc1341c262a0
Jake Cracknell <drspa@ymail.com>
2018-12-29 23:32:02 +0000

    added test

--- src/test/java/com/cracknellj/fare/provider/AtocDataProviderTest.java
+++ src/test/java/com/cracknellj/fare/provider/AtocDataProviderTest.java
@@ -21,7 +21,6 @@ public class AtocDataProviderTest {
             Map<String, FareSet> allFareSets = new AtocDataProvider().getAllFareSets();
             System.out.println("Time elapsed: " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + ", FareSet size: " + allFareSets.size());
         }
-
     }
 
     @Test
@@ -42,6 +41,29 @@ public class AtocDataProviderTest {
                     if (pricePerKm < cheapestPricePerKm && pricePerKm != 0 && !fareDetail.get().isTFL) {
                         cheapestPricePerKm = pricePerKm;
                         System.out.printf("£%.4f/km,%s,%s,%s%n", cheapestPricePerKm, stationFrom, stationTo, fareDetail.get());
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    public void findUnfairReverseJourneys() throws Exception {
+        FareDataProvider fareDataProvider = CompositeFareDataProvider.load();
+        List<Station> stations = StationFileReader.getStations();
+
+        int biggestFareDifference = 0;
+        for (Station stationFrom : stations) {
+            for (Station stationTo : stations) {
+                FareDetail fareDetail1 = fareDataProvider
+                        .getFares(stationFrom.stationId, stationTo.stationId).cheapestPeak;
+                FareDetail fareDetail2 = fareDataProvider
+                        .getFares(stationTo.stationId, stationFrom.stationId).cheapestPeak;
+                if (fareDetail1 != null && fareDetail2 != null && fareDetail1.price != fareDetail2.price) {
+                    int difference = Math.abs(fareDetail1.price - fareDetail2.price);
+                    if (difference > biggestFareDifference) {
+                        biggestFareDifference = difference;
+                        System.out.printf("%dp,%s,%s%n", difference, stationFrom, stationTo);
                     }
                 }
             }
